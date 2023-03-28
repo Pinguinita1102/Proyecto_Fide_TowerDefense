eb48f819c56f9008b552212cd315de977c1d3fce
Valeria_Arias_Castro <107090508+Pinguinita1102@users.noreply.github.com>
2023-03-27 22:06:15 -0600

    Feat: Diseño de Interfaz P1

--- Proyecto_Fide_TowerDefense/src/main/java/com/mycompany/proyecto_fide_towerdefense/Proyecto_Fide_TowerDefense.java
+++ Proyecto_Fide_TowerDefense/src/main/java/com/mycompany/proyecto_fide_towerdefense/Proyecto_Fide_TowerDefense.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
 
 package com.mycompany.proyecto_fide_towerdefense;
 
@@ -12,6 +8,7 @@ package com.mycompany.proyecto_fide_towerdefense;
 public class Proyecto_Fide_TowerDefense {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        VentanaPrincipal main = new VentanaPrincipal();//Llamamos al JFrame
+        main.setVisible(true);//Esto es para mostrar el JFrame de VentanaPrincipal
     }
 }
